9c3ecd3bb16334c8a5487441040ddab3dee7ee90
Vulcan <vulcan@Vulcans-MacBook-Air-6.local>
2025-04-15 11:00:33 +0700

    taipush

--- src/testdemo.java
+++ src/testdemo.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginAutomation {
+public class testdemo {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/vulcan/Downloads/chromedriver-mac-arm64/chromedriver");
